3a7511db7bd5429525fbbf06ac548d82d8e68f5e
James Chartrand <jc.chartrand@gmail.com>
2023-11-19 20:19:42 -0500

    test

--- dist/index.js
+++ dist/index.js
@@ -30700,7 +30700,13 @@ const main = async () => {
 }
 
 async function getManifestSha(owner, repo, path, octokit) {
-    const { data: { sha } } = await octokit.rest.repos.getContent({owner, repo, path})
+  let sha
+  try {
+    const response = await octokit.rest.repos.getContent({owner, repo, path})
+    sha = response.data.sha
+  } catch (e) {
+    console.log(`Problem saving file ${path} back to the Github repository ${owner}/${repo}: ${e}`);
+  }
     return sha
 }
 
