083834d2285709c61f8ed91b1bb3071963c10f3d
James Chartrand <jc.chartrand@gmail.com>
2024-01-25 09:38:52 -0500

    fix

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const main = async () => {
     //octokit.rest.git.getCommit({owner, repo, commit_sha});
    
     
-    const collectionFileAsString = dtsUtils.createDTSCollection(owner, repo, octokit)
+    const collectionFileAsString = await dtsUtils.createDTSCollection(owner, repo, octokit)
     saveFileToGithub(owner, repo, collectionFileAsString, octokit)
 
 
